87be3a31a366b13b7d790e4505b8135358bce2c6
gledson <gletech84@gmail.com>
2026-05-06 06:48:07 -0300

    Update jwt.py

--- auth/jwt.py
+++ auth/jwt.py
@@ -1,1 +1,21 @@
 
+import jwt
+import datetime
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+def gerar_token(user_id):
+    payload = {
+        "user_id": user_id,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(days=7)
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+
+def validar_token(token):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return payload["user_id"]
+    except:
+        return None
