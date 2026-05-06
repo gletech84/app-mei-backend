2182ddd2340319dd62d349a76d40341655cc49f8
gledson <gletech84@gmail.com>
2026-05-06 14:03:36 -0300

    Update pagamento_service.py

--- app/services/pagamento_service.py
+++ app/services/pagamento_service.py
@@ -1,1 +1,20 @@
+import mercadopago
+import os
 
+MP_ACCESS_TOKEN = os.getenv("MP_ACCESS_TOKEN")
+
+sdk = mercadopago.SDK(MP_ACCESS_TOKEN)
+
+
+def criar_pagamento(email):
+    payment_data = {
+        "transaction_amount": 19.90,
+        "description": "Plano Premium MEI",
+        "payment_method_id": "pix",
+        "payer": {
+            "email": email
+        }
+    }
+
+    payment = sdk.payment().create(payment_data)
+    return payment["response"]
