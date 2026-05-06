c22984076245fd8ce65e3d91c893bdb8969c6c49
gledson <gletech84@gmail.com>
2026-05-06 18:47:50 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,27 @@
+import sqlite3
 
+def init_db():
+    conn = sqlite3.connect("saas.db")
+    c = conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT,
+        senha TEXT,
+        plano TEXT,
+        ativo INTEGER DEFAULT 0
+    )
+    """)
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS pagamentos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        txid TEXT,
+        status TEXT,
+        user_id INTEGER
+    )
+    """)
+
+    conn.commit()
+    conn.close()
