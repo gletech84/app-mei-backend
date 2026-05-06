18b2cba077043b3291aeff90138eed0052bb6fbb
gledson <gletech84@gmail.com>
2026-05-06 06:36:13 -0300

    Update user_repository.py

--- users/user_repository.py
+++ users/user_repository.py
@@ -1,1 +1,44 @@
+from database.db import get_conn
 
+def criar_usuario(email):
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("""
+        INSERT INTO users (email)
+        VALUES (%s)
+        ON CONFLICT (email) DO NOTHING
+    """, (email,))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def buscar_usuario(email):
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("SELECT id, email, plano, assinatura_ativa FROM users WHERE email=%s", (email,))
+    user = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return user
+
+
+def ativar_assinatura(email):
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("""
+        UPDATE users
+        SET assinatura_ativa = TRUE,
+            plano = 'pro'
+        WHERE email=%s
+    """, (email,))
+
+    conn.commit()
+    cur.close()
+    conn.close()
