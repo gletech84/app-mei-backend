66471d09728782b8f928b9bb85b4396e02876e89
gledson <gletech84@gmail.com>
2026-05-06 19:03:05 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,11 @@
+from flask import session
+from database import get_user
 
+def login_user(email, senha):
+    user = get_user(email, senha)
+
+    if user:
+        session["user_id"] = user[0]
+        return True
+
+    return False
