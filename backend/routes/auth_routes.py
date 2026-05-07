d9108fcfd1b02443d9fecae894221b349deda69c
gledson <gletech84@gmail.com>
2026-05-07 08:30:07 -0300

    Update auth_routes.py

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,1 +1,21 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
 
+router = APIRouter()
+
+class LoginModel(BaseModel):
+    email: str
+    senha: str
+
+
+@router.post("/login")
+def login(data: LoginModel):
+
+    if data.email == "admin" and data.senha == "123":
+        return {
+            "user_id": "1",
+            "token": "token_demo",
+            "status": "ok"
+        }
+
+    return {"error": "invalid login"}
